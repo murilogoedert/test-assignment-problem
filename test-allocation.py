1027ff4c53a455adae947529f0b02b0be2cb69f9
Murilo Goedert <33994399+murilogoedert@users.noreply.github.com>
2022-07-14 20:42:37 -0300

    Add files via upload

--- test-allocation.py
+++ test-allocation.py
@@ -1,29 +1,33 @@
 
-import sys, re, numpy
+import sys, re, random
 
 def readInstance():
-    file = open(sys.argv[1])
+    if(len(sys.argv) > 1):
+        file = open(sys.argv[1])
+    else:
+        file = open('instances/lab6b_2x20.txt')
+        
     lines = file.readlines()
     sub = re.sub(' +', ' ', re.sub('\n', '', re.sub('\t', ' ', lines[0])));
     instanceParts = sub.split(' ')
 
-    numNodes = int(instanceParts[0])
-    numEdges = int(instanceParts[1])
+    numDesks = int(instanceParts[0])
+    numDistances = int(instanceParts[1])
     numTests = int(instanceParts[2])
     numEmptyDesks = int(instanceParts[3])
 
     countLines = 0
     desks = []
 
-    while(countLines < numNodes):
+    while(countLines < numDesks):
         desks.append(int(lines[countLines + 1]))
         countLines += 1
 
-    distanceMatrix = numpy.full((numNodes, numNodes), 0, dtype=float)
+    distanceMatrix = createMatrix(numDesks, numDesks, None)
 
     countLines += 1
 
-    while(countLines < (numEdges + numNodes + 1)):
+    while(countLines < (numDistances + numDesks + 1)):
         currentLine = lines[countLines]
         lineParts = re.sub(' +', ' ', re.sub('\n', '', re.sub('\t', '', currentLine))).split(' ');
         i = desks.index(int(lineParts[0]))
@@ -32,9 +36,9 @@ def readInstance():
         distanceMatrix[i][j] = value
         countLines += 1
 
-    similarityMatrix = numpy.full((numTests, numTests), 0, dtype=float)
+    similarityMatrix = createMatrix(numTests, numTests)
 
-    while(countLines < numEdges + numNodes + 1 + (numTests * 2)):
+    while(countLines < numDistances + numDesks + 1 + (numTests * 2)):
         currentLine = lines[countLines]
         lineParts = re.sub(' +', ' ', re.sub('\n', '', re.sub('\t', '', currentLine))).split(' ');
         i = int(lineParts[0])
@@ -43,11 +47,51 @@ def readInstance():
         similarityMatrix[i][j] = value
         countLines += 1
 
-    return numNodes, numEdges, numTests, numEmptyDesks, distanceMatrix, similarityMatrix;
+    return desks, numDistances, numTests, numEmptyDesks, distanceMatrix, similarityMatrix
+
+def createMatrix(lines, columns, defValue = 0):
+    matrix = []
+    for i in range(lines):
+        line = []
+        for j in range(columns):
+            line.append(0)
+        matrix.append(line)
+    return matrix
+
+def solucaoInicial(desks, numTests, numEmptyDesks):
+    solution = []
+    for i in range(len(desks)):
+        solution.append(0)
+
+    while solution.count(0) > numEmptyDesks:
+        solution[random.randint(0, len(desks) - 1)] = random.randint(1, numTests)
+
+    return solution
+            
+        
+def avaliaSolucao(distanceMatrix, similarityMatrix, solution):
+    totalCost = 0
+    for i in range(len(solution) - 1):
+        for j in range(i + 1, len(solution)):
+            if(solution[i] != 0 and solution[j] != 0):
+                totalCost += similarityMatrix[solution[i] - 1][solution[j] - 1] * distanceMatrix[i][j]
 
     
+    return totalCost
 
-print(readInstance())
+
+def buscaLocal():
+    desks, numDistances, numTests, numEmptyDesks, distanceMatrix, similarityMatrix = readInstance()
+    solucaoAtual = solucaoInicial(desks, numTests, numEmptyDesks)
+    print(avaliaSolucao(distanceMatrix, similarityMatrix, solucaoAtual))
+
+
+
+buscaLocal()
+
+
+
+
 
 #  python .\test-allocation.py instances/lab1_2x10.txt 
 
